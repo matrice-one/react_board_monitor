e9ad04367c0726cb187a45deb6e53692d98d3930
matrice-one <neigeline.rivollat@gmail.com>
2023-06-13 13:17:45 +0200

    suggested correction error

--- src/components/StyledSearchBar.js
+++ src/components/StyledSearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { TextField, InputAdornment, Button, Box } from '@mui/material';
 import Autocomplete from '@mui/material/Autocomplete';
 import debounce from 'lodash.debounce';
@@ -7,13 +7,13 @@ const SearchBar = ({ inputMessage, buttonMessage, onSearch }) => {
   const [search, setSearch] = useState('');
   const [suggestions, setSuggestions] = useState([]);
 
-  const fetchSuggestions = async (query) => {
-    const response = await fetch(`http://127.0.0.1:8000/api/companies/?query=${query}`);
+  const fetchSuggestions = useCallback(async (query) => {
+    const response = await fetch(`http://board-visualizer/api/companies/?query=${query}`);
     const data = await response.json();
     setSuggestions(data);
-  };
+  }, []);
 
-  const debouncedFetchSuggestions = debounce(fetchSuggestions, 300);
+  const debouncedFetchSuggestions = useCallback(debounce(fetchSuggestions, 300), [fetchSuggestions]);
 
   useEffect(() => {
     if (search) {
@@ -21,7 +21,7 @@ const SearchBar = ({ inputMessage, buttonMessage, onSearch }) => {
     } else {
       setSuggestions([]);
     }
-  }, [search]);
+  }, [search, debouncedFetchSuggestions]);
 
   const handleInputChange = (event, value, reason) => {
     if (reason === 'input') {
